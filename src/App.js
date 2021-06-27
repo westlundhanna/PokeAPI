7a31011bbb5bea9f7e1bfab66c8751304eda7c8e
westlundhanna <westlund90@hotmail.com>
2021-06-27 19:36:02 +0200

    removed list component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
-import PokemonList from './components/PokemonList';
+import './App.css';
+import Pokemon from './components/Pokemon';
 
-const url = 'https://pokeapi.co/api/v2/pokemon'
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 function App() {
-    const { data: result, error } = useSWR(url, fetcher);
-    
-    if (error) return <div>failed to load</div>
-    if (!result) return <div>loading...</div>
-    console.log(result);
-    return (
-      
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=20`;
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json())
+  const { data: result, error } = useSWR(url, fetcher);
+  
+
+  if (error) return <div>failed to load</div>
+  if (!result) return <div>loading...</div>
+  console.log(result);
+  return (
+    <div>
       <div className="pokemons">
-          {result.results.map((pokemon) => (
-              <PokemonList key={pokemon.name} pokemon={pokemon} />
-              
-          ))}
-          
+        {result.results.map((pokemon) => (
+          <Pokemon key={pokemon.name} pokemon={pokemon} />
+
+        ))}
+
       </div>
-    )
-    
+    </div>
+  )
+
 }
 export default App
