18149fd2eb6d2fc0d4e5cc95f587f99cae83e011
westlundhanna <westlund90@hotmail.com>
2021-06-28 13:02:58 +0200

    added toUpperCase() of first letter of pokemon name

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -12,11 +12,12 @@ function Pokemon({ pokemon }) {
 
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
-    console.log(data.types[0].type.name);
+
     return (
         <div className='pokemon-card'>
             <div className='pokemon-card__content'>
-                <h1 className='pokemon-card__name'>{name}</h1>
+                <p>{data.id}</p>
+                <h1 className='pokemon-card__name'>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
                 <img
                     className='pokemon-card__image'
                     src={data.sprites.front_default}
