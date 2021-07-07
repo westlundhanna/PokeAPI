d7db226ba63dbeef725d5da7f7386848f45e4e04
westlundhanna <westlund90@hotmail.com>
2021-07-07 18:29:07 +0200

    removed api call from app.js and added routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { useState } from 'react';
-import useSWR from 'swr';
+import React from 'react';
+import PokemonList from './components/PokemonList';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+  } from "react-router-dom";
+import PokemonDetails from './components/PokemonDetails';
 import './App.css';
-import Pokemon from './components/Pokemon';
-
 
 function App() {
-  
-  const url = `https://pokeapi.co/api/v2/pokemon/?limit=8&offset=0`;
-
-  const fetcher = (...args) => fetch(...args).then((res) => res.json())
-  const { data: result, error } = useSWR(url, fetcher);
-
-  if (error) return <div>failed to load</div>
-  if (!result) return <div>loading...</div>
-
+ 
   return (
     <div>
-      <div className="pokemons">
-
-        {result.results.sort((a, b) => a.name < b.name ? -1 :1).map((pokemon) => (
-          <Pokemon key={pokemon.name} pokemon={pokemon} />
-
-        ))}
-
-      </div>
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <PokemonList /> 
+          </Route>
+          <Route exact path="/:pokemonName">
+            <PokemonDetails  />
+          </Route>
+        </Switch>
+      </Router>
     </div>
+
   )
 
 }
