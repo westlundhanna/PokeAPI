8f5d24a2565305204affaee3c6efec594f2e1864
westlundhanna <westlund90@hotmail.com>
2021-07-19 18:54:54 +0200

    added some pokemon images via props

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,20 +1,32 @@
-import { useParams, useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import React from "react";
 
-const PokemonDetails = () => {
-    const params = useParams();
-
+const PokemonDetails = (props) => {
+    
     let history = useHistory();
 
     function clickHandler() {
         history.push("/");
     }
 
+    console.log(props.location.pokemonProps)
+    console.log(props.location.pokemonProps.name)
+
     return (
-        <>
-            
-            <p>{params.pokemonName}</p>
+        <div className="pokemon-details">
+            <h1>{props.location.pokemonProps.name}</h1>
+            <img
+                className='pokemon-card__image'
+                src={props.location.pokemonProps.sprites.front_default}
+                alt={props.location.pokemonProps.name}
+            />
+            <img
+                className='pokemon-card__image'
+                src={props.location.pokemonProps.sprites.back_default}
+                alt={props.location.pokemonProps.name}
+            />
             <button onClick={clickHandler}>Go back</button>
-        </>
+        </div>
     )
 
 
