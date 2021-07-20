f38b4a825919ce53b2e1dd82e9477625bd85b62d
westlundhanna <westlund90@hotmail.com>
2021-07-20 19:50:58 +0200

    modified html struture

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -20,15 +20,16 @@ const PokemonCard = ({ pokemon }) => {
         }}>
         <div className='pokemon-card'>
             <div className='pokemon-card__content'>
-                <p>{data.id}</p>
-                <h1 className='pokemon-card__name'>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
                 <img
                     className='pokemon-card__image'
                     src={data.sprites.front_default}
                     alt={name}
                 />
-                <p>{data.abilities[0].ability.name}</p>
-                <p>{data.types[0].type.name}</p>
+                <div className='pokemon-card__info'>
+                    <p className='pokemon-card__name'>Name: {name.charAt(0).toUpperCase() + name.slice(1)}</p>
+                    <p>Abilities: {data.abilities[0].ability.name.charAt(0).toUpperCase() + data.abilities[0].ability.name.slice(1)}</p>
+                    <p>Category: {data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1)}</p>
+                </div>
             </div>
         </div>
         </Link>
