d83c40e06bc40e7d9766eb927a32d3c4ab84523c
westlundhanna <westlund90@hotmail.com>
2021-07-20 19:46:31 +0200

    added api call to endpoint pokemon-species

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,31 +1,47 @@
 import { useHistory, useLocation } from "react-router-dom";
 import React from "react";
+import useSWR from "swr";
+import './PokemonDetails.css';
 
 const PokemonDetails = (props) => {
-    
-    let history = useHistory();
+
+    const history = useHistory();
+
+    const name = props.location.pokemonProps.name;
+
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${name}`;
+
+    const fetcher = (...args) => fetch(...args).then((res) => res.json())
+    const { data: result, error } = useSWR(url, fetcher);
+  
+    if (error) return <div>failed to load</div>
+    if (!result) return <div>loading...</div>
+
+    const description = result.flavor_text_entries[0].flavor_text.replace(/[^a-zA-Z.é ]/g, " ");
 
     function clickHandler() {
         history.push("/");
     }
 
-    console.log(props.location.pokemonProps)
-    console.log(props.location.pokemonProps.name)
-
     return (
         <div className="pokemon-details">
-            <h1>{props.location.pokemonProps.name}</h1>
-            <img
-                className='pokemon-card__image'
-                src={props.location.pokemonProps.sprites.front_default}
-                alt={props.location.pokemonProps.name}
-            />
-            <img
-                className='pokemon-card__image'
-                src={props.location.pokemonProps.sprites.back_default}
-                alt={props.location.pokemonProps.name}
-            />
-            <button onClick={clickHandler}>Go back</button>
+            <div className="pokemon-details__content">
+                <h1>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
+                <div className="pokemon-details__images">
+                    <img
+                        className='pokemon-card__image'
+                        src={props.location.pokemonProps.sprites.front_default}
+                        alt={props.location.pokemonProps.name}
+                    />
+                    <img
+                        className='pokemon-card__image'
+                        src={props.location.pokemonProps.sprites.back_default}
+                        alt={props.location.pokemonProps.name}
+                    />
+                </div>
+                <p>{description}</p>
+            </div>
+            <button className="go-back" onClick={clickHandler}>Go back</button>
         </div>
     )
 
