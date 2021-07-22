75bb1527c5b6728257af075abe77fd166205d6bb
westlundhanna <westlund90@hotmail.com>
2021-07-22 20:57:10 +0200

    added values to details from api endpoint pokemon-species

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,4 +1,4 @@
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import React from "react";
 import useSWR from "swr";
 import './PokemonDetails.css';
@@ -17,16 +17,18 @@ const PokemonDetails = (props) => {
     if (error) return <div>failed to load</div>
     if (!result) return <div>loading...</div>
 
-    const description = result.flavor_text_entries[0].flavor_text.replace(/[^a-zA-Z.é ]/g, " ");
+    const description = result.flavor_text_entries[0].flavor_text.replace(/[^a-zA-Z.é´' ]/g, " ");
+    const eggGroup = result.egg_groups[0].name;
+    const color = result.color.name;
 
     function clickHandler() {
         history.push("/");
     }
 
     return (
-        <div className="pokemon-details">
+        <div className="pokemon-details__wrapper">
             <div className="pokemon-details__content">
-                <h1>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
+                <h1>{result.id}. {name.charAt(0).toUpperCase() + name.slice(1)}</h1>
                 <div className="pokemon-details__images">
                     <img
                         className='pokemon-card__image'
@@ -39,14 +41,21 @@ const PokemonDetails = (props) => {
                         alt={props.location.pokemonProps.name}
                     />
                 </div>
-                <p>{description}</p>
+                <ul className="pokemon-details__list">
+                    <li><h4>Egg group</h4>
+                        <p>{eggGroup}</p>
+                    </li>
+                    <li><h4>Color</h4>
+                        <p>{color}</p>
+                    </li>
+                    <li><h4>Description</h4>
+                        <p className="description">{description}</p>
+                    </li>
+                </ul>
             </div>
             <button className="go-back" onClick={clickHandler}>Go back</button>
         </div>
     )
-
-
-
 }
 
 export default PokemonDetails;
