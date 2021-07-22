73f2822d529e32117c7e11b8b356924c7ecb55ab
westlundhanna <westlund90@hotmail.com>
2021-07-22 20:54:16 +0200

    added pagination to pokemon list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,28 +1,31 @@
-import React from "react";
-import PokemonCard from './PokemonCard';
+import { useState } from 'react';
 import useSWR from 'swr';
+import PokemonCard from './PokemonCard';
 
 const PokemonList = () => {
+
+    const [index, setIndex] = useState(0);
     
-    const url = `https://pokeapi.co/api/v2/pokemon/?limit=8`;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=9&offset=${index}`;
 
     const fetcher = (...args) => fetch(...args).then((res) => res.json())
     const { data: result, error } = useSWR(url, fetcher);
-  
+
     if (error) return <div>failed to load</div>
     if (!result) return <div>loading...</div>
-    
 
     return (
-    <div>
-      <div className="pokemons">
-          {result.results.sort((a, b) => a.name < b.name ? -1 :1).map((pokemon) => (
-              <PokemonCard key={pokemon.name} pokemon={pokemon} /> 
-          ))}
-
-      </div>
-      
-    </div>
+        <div className="pokemon-list">
+            <div className="pokemons">
+                {result.results.sort((a, b) => a.name < b.name ? -1 : 1).map((pokemon) => (
+                    <PokemonCard key={pokemon.name} pokemon={pokemon} />
+                ))}
+            </div>
+            <div className="pagination">
+                <button onClick={() => setIndex(index - 9)}>Previous</button>
+                <button onClick={() => setIndex(index + 9)}>Next</button>
+            </div>
+        </div>
     )
 }
 
