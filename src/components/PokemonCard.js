4625372ba76bbc74eff2b63a0c2f87de60b29697
westlundhanna <westlund90@hotmail.com>
2021-09-01 20:23:23 +0200

    changed so Link doesn't wrap the div + css

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -14,26 +14,27 @@ const PokemonCard = ({ pokemon }) => {
     if (!data) return <div>loading...</div>
 
     return (
-        <Link to={{
-            pathname: `/${data.name}`,
-            pokemonProps: data
-        }}>
-        <div className='pokemon-card'>
-            <div className='pokemon-card__content'>
-                <img
-                    className='pokemon-card__image'
-                    src={data.sprites.front_default}
-                    alt={name}
-                />
-                <div className='pokemon-card__info'>
+            <div className='pokemon-card'>
+                <div className='pokemon-card__content'>
+                    <img
+                        className='pokemon-card__image'
+                        src={data.sprites.front_default}
+                        alt={name}
+                    />
+                    <div className='pokemon-card__info'>  
                     <p className='pokemon-card__name'>Name: {name}</p>
                     <p className='pokemon-card__abilities'>Abilities: {data.abilities[0].ability.name}</p>
                     <p className='pokemon-card__categories'>Category: {data.types[0].type.name}</p>
+                    </div>
                 </div>
+                <Link className='pokemon-card__link' to={{
+                    pathname: `/${name}`,
+                    pokemonProps: data,
+                    state: pokemon
+                    }}>
+                    View Details
+                </Link>
             </div>
-        </div>
-        </Link>
-
     )
 }
 
